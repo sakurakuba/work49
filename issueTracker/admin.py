d45ae92ffcc9388637b78c36217cd0b4c6b4da0e
Kuba <ksulaymanov@gmail.com>
2022-07-15 01:31:36 -0500

    added models for issue, status and type. added superuser

--- issueTracker/admin.py
+++ issueTracker/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from issueTracker.models import Issue, Status, Type
+
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'status', 'type', 'created_at']
+    list_display_links = ['summary']
+    list_filter = ['status']
+    search_fields = ['summary', 'status']
+    fields = ['summary', 'description', 'status', 'type', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Issue, IssueAdmin)
--- issueTracker/models.py
+++ issueTracker/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Create date')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated date')
+
+    class Meta:
+        abstract = True
+
+
+class Issue(BaseModel):
+    summary = models.CharField(max_length=100, verbose_name='summary')
+    description = models.TextField(max_length=800, null=True, blank=True, verbose_name='description', default='not specified')
+    status = models.ForeignKey("issueTracker.Status", on_delete=models.PROTECT, related_name='statuses', verbose_name='status')
+    type = models.ForeignKey("issueTracker.Type", on_delete=models.PROTECT, related_name='types',  verbose_name='type')
+
+    def __str__(self):
+        return f"{self.id}. {self.summary} - status: {self.status}, type: {self.type}"
+
+    class Meta:
+        db_table = 'issues'
+        verbose_name = 'Issue'
+        verbose_name_plural = 'Issues'
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=50, verbose_name='status')
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=50, verbose_name='type')
+
+    def __str__(self):
+        return f"{self.name}"
+
