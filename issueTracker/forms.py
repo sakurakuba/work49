4deec50027d80aa8d2146aca278b1a11317109f9
Kuba <ksulaymanov@gmail.com>
2022-07-19 01:45:12 -0500

    updated form.py to use ModelFor via Meta. added 1 validation rule for summary field

--- issueTracker/forms.py
+++ issueTracker/forms.py
@@ -1,15 +1,28 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.forms import widgets
 from django.forms import ModelMultipleChoiceField
 
 from .models import Issue, Type, Status
 
 
 class IssueForm(forms.ModelForm):
-    summary = forms.CharField(max_length=50, required=True, label='Summary')
-    description = forms.CharField(max_length=300, required=False, label='Description')
-    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), label='Type')
-    status = forms.ModelChoiceField(queryset=Status.objects.all(), label='Status')
+    # summary = forms.CharField(max_length=50, required=True, label='Summary')
+    # description = forms.CharField(max_length=300, required=False, label='Description')
+    # type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), label='Type')
+    # status = forms.ModelChoiceField(queryset=Status.objects.all(), label='Status')
 
     class Meta:
         model = Issue
         fields = "__all__"
+        widgets = {
+            "type": widgets.CheckboxSelectMultiple,
+            "description": widgets.Textarea(attrs={"placeholder": "please add text here"})
+        }
+
+    def clean_summary(self):
+        summary = self.cleaned_data.get('summary')
+        if len(summary) > 20:
+            raise ValidationError("Summary too long")
+        return summary
+
