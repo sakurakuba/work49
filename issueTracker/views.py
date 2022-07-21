51677449950e3f31336fe18f61f67ccc07be151a
Kuba <ksulaymanov@gmail.com>
2022-07-21 01:20:41 -0500

    added 'search' field to main page

--- issueTracker/views.py
+++ issueTracker/views.py
@@ -1,12 +1,14 @@
+from django.db.models import Q
 from django.forms import forms
 from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from django.urls import reverse
+from django.utils.http import urlencode
 from django.views import View
 from django.views.generic import TemplateView, FormView, ListView
 
-from issueTracker.forms import IssueForm
+from issueTracker.forms import IssueForm, SearchForm
 from issueTracker.models import Issue, Type, Status
 
 
@@ -20,10 +22,34 @@ class IndexView(ListView):
     model = Issue
     template_name = "index.html"
     context_object_name = "issues"
-    ordering = ('created_at',)
+    ordering = ["-created_at"]
     paginate_by = 5
 
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
 
+    def get_queryset(self):
+        if self.search_value:
+            return Issue.objects.filter(Q(summary__contains=self.search_value) | Q(description__contains=self.search_value))
+        return Issue.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form'] = self.form
+        if self.search_value:
+            val = urlencode({'search': self.search_value})
+            context['query'] = val
+            context['search'] = self.search_value
+        return context
+
+    def get_search_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data.get("search")
 
 
 class IssueView(TemplateView):
