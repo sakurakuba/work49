43f42e2121a3d57459c9e4cb421debd9a59f0e8b
Kuba <ksulaymanov@gmail.com>
2022-07-25 21:08:23 -0500

    added Project to models

--- issueTracker/models.py
+++ issueTracker/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.datetime_safe import date
 
 
 class BaseModel(models.Model):
@@ -16,6 +17,7 @@ class Issue(BaseModel):
     description = models.TextField(max_length=800, null=True, blank=True, verbose_name='description', default='not specified')
     status = models.ForeignKey("issueTracker.Status", on_delete=models.PROTECT, related_name='statuses', verbose_name='status')
     type = models.ManyToManyField("issueTracker.Type",  related_name='types')
+    ##project = models.ForeignKey("issueTracker.Project", on_delete=models.CASCADE, related_name='projects', verbose_name='project')
 
     def __str__(self):
         return f"{self.id}. {self.summary} - status: {self.status}, type: {self.type.all()}"
@@ -38,4 +40,19 @@ class Type(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Project(models.Model):
+    start_date = models.DateField(default=date.today, verbose_name='Start date')
+    end_date = models.DateField(blank=True, default='', verbose_name='End date')
+    project_name = models.CharField(max_length=50, verbose_name='Project name')
+    project_description = models.TextField(max_length=1000, verbose_name='Project description', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.id}. {self.project_name} - start date: {self.start_date}, end date: {self.end_date}"
+
+    class Meta:
+        db_table = 'projects'
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
 
