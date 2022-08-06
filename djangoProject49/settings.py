0e86791e517a062bd9cbbb19bf447405f7bf6e1b
Kuba <ksulaymanov@gmail.com>
2022-08-06 15:02:07 -0500

    added login and logout functionality. created login page. added conditions for creating new project if authorised

--- djangoProject49/settings.py
+++ djangoProject49/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_URL = 'accounts:login'
+LOGOUT_REDIRECT_URL = "issueTracker:index"
+LOGIN_REDIRECT_URL = "issueTracker:index"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
