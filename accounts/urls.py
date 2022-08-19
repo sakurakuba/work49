bea59141dcc67c4ad993ed53df82eaef19c97f14
Kuba <ksulaymanov@gmail.com>
2022-08-19 00:28:34 -0500

    implemented functionality to see personal profile and assigned projects. created profile and project_list pages. updated base page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import RegisterView
+from accounts.views import RegisterView, ProfileView
 
 app_name = "accounts"
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name="login.html"), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('registration/', RegisterView.as_view(), name='registration'),
+    path('<int:pk>/', ProfileView.as_view(), name='profile')
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from accounts.forms import MyUserCreationForm
 
 
@@ -25,3 +27,27 @@ class RegisterView(CreateView):
         if not next_url:
             next_url = reverse('issueTracker:index')
         return next_url
+
+
+# class ProfileView(LoginRequiredMixin, DetailView):
+#     model = get_user_model()
+#     template_name = "profile.html"
+
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = "profile.html"
+    paginate_by = 6
+    paginate_orphans = 0
+
+    def get_context_data(self, **kwargs):
+        paginator = Paginator(self.get_object().projects.all(),
+                              self.paginate_by,
+                              self.paginate_orphans)
+        page_number = self.request.GET.get('page', 1)
+        page_object = paginator.get_page(page_number)
+        context = super().get_context_data(**kwargs)
+        context['page_obj'] = page_object
+        context['projects'] = page_object.object_list
+        context['is_paginated'] = page_object.has_other_pages()
+        return context
